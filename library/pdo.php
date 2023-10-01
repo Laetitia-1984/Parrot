5ae6da0d1773bd89e14cc028c5a9e512764be03a
Laetitia Bouslah <laetitia.bouslah@free.fr>
2023-10-01 19:06:39 +0200

    changement pdo.php

--- library/pdo.php
+++ library/pdo.php
@@ -1,3 +1,3 @@
 <?php
 //Parametres de connexion vers la bdd
-$pdo = new PDO('mysql:dbname=garage;port=3307;host=localhost','vincent','V.Parrot1953');
+$pdo = new PDO('mysql:dbname=garage-parrot-vincent_garage;port=3307;host=localhost','vincent','V-Parrot-1953');
