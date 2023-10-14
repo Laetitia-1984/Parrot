15bfa4d4172fcf714b2312976f3ad1536d1a1c66
Laetitia Bouslah <laetitia.bouslah@free.fr>
2023-10-14 21:40:46 +0200

    ajout des metas

--- templates/header.php
+++ templates/header.php
@@ -10,6 +10,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="voitures, garages, occasions"/>
+    <meta name="description" content="Un garage au service de votre voiture."/>
     <title>Garage Parrot</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/override-bootstrap.css">
