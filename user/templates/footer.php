ba5f2ac98460320c5a0c6189c2e30d98aabe8041
Laetitia Bouslah <laetitia.bouslah@free.fr>
2023-10-16 17:50:09 +0200

    modif footer user

--- user/templates/footer.php
+++ user/templates/footer.php
@@ -8,14 +8,10 @@
                 <a href="index.php" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <img src="assets/images/Logo.png" alt="logo garage" width="100">
                 </a>
-                <div class="row mx-auto my-3">
-                    <?php foreach ($hours as $key => $hour) { ?>
-                        <div class="col">
-                            <?php 
-                                include('templates/hours_partial.php');
-                            ?>
-                        </div>
-                    <?php } ?>
+                <div class="col-md-4 mb-0">
+                    <?php foreach ($hours as $key => $hour) { 
+                        include('../templates/hours_partial.php');
+                    } ?>
                 </div>
             </footer>
         </main>
